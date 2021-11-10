1b95dea5f3e1a8ce36779ecf1b243b193ef7d9fe
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-11-10 00:54:33 -0500

    renders list of friends

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,10 +1,28 @@
-import React, { useHistory, useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const FriendsList = () => {
+  const [friends, setFriends] = useState([]);
 
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/friends")
+      .then((resp) => {
+        setFriends(resp.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return <div>Friends List
 
-    return(<div>Friends List</div>);
+{friends.map(friend => (
+    <div><div>{friend.name}</div>
+    <div>{friend.age}</div>
+    <div>{friend.email}</div></div>
+))}
+
+  </div>;
 };
 
 export default FriendsList;
